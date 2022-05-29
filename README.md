f95f69c435fea4ed1f36fcc61e917d609f84f566
Akshat Sethi <akshat@nova-benefits.com>
2022-05-29 23:42:00 +0530

    done

---
+++ README.md
@@ -0,0 +1,5 @@
+# JSX-Classes
+
+In this excercise we will add a class to an html div in JSX.
+In <code>src/index.js</code>, we have constant with some jsx.
+Add the class <code>jsx-class</code>  to the div.
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+
+const JSX = (
+    <div>
+      <h1>Add a class to this div</h1>
+    </div>
+);
+
+
+ReactDOM.render(JSX, document.getElementById("root"));
